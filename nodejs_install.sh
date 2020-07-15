e41da3bdd59c451aa8d0415d30c5f1254740009c
Ashwin M.A <ashwin.m@RG-LAP-000240.local>
2020-07-15 22:40:35 +0530

    update

--- nodejs_install.sh
+++ nodejs_install.sh
@@ -9,50 +9,62 @@ if [ `whoami` != root ]; then
 else    
 
      read -p "Past URL to Install : " url
+        u=`echo $url | grep -o '\b\w*linux-x64\w*\b'`
 
-    if  curl --output /dev/null --silent --head --fail "$url"; then
-        
-        i=`echo $url | sed 's|.*/||'` 
-        z=`echo $url | sed 's|.*tar.||'`
 
-        if [ "$z" = xz ]
-        then
+        if  curl --output /dev/null --silent --head --fail "$url"; then
+                
+            if [ "$u" = linux-x64 ]
+            then
+                echo "OK"
 
-            curl -o $i $url
-            tar -C /usr/local --strip-components 1 -xf $i
-            npm install -g npm@latest
-            rm -rf $i*    
+                i=`echo $url | sed 's|.*/||'` 
+                z=`echo $url | sed 's|.*tar.||'`
+                # echo $i
 
-            NODE_VER=$(node -v)
-            NPM_VER=$(npm -v)
+                if [ "$z" = xz ]
+                then
 
-            echo -ne " Nodejs" "\033[0;32m$NODE_VER\033[0m" "Installed Sucessfully"
-            echo -ne " Npm" "\033[0;32m$NPM_VER\033[0m" "Installed Sucessfully"
+                    curl -o $i $url
+                    tar -C /usr/local --strip-components 1 -xf $i
+                    npm install -g npm@latest
 
-        elif [ "$z" = gz ] 
-        then
+                    NODE_VER=$(node -v)
+                    NPM_VER=$(npm -v)
 
-            curl -o $i $url
-            tar -C /usr/local --strip-components 1 -xzf $i 
-            npm install -g npm@latest
-            rm -rf $i*
+                    echo -ne " Nodejs" "\033[0;32m$NODE_VER\033[0m" "Installed Sucessfully"
+                    echo -ne " Npm" "\033[0;32m$NPM_VER\033[0m" "Installed Sucessfully"
 
-            NODE_VER=$(node -v)
-            NPM_VER=$(npm -v)
+                elif [ "$z" = gz ] 
+                then
 
-            echo -ne " Nodejs" "\033[0;32m$NODE_VER\033[0m" "Installed Sucessfully"
-            echo -ne " Npm" "\033[0;32m$NPM_VER\033[0m" "Installed Sucessfully"
+                    curl -o $i $url
+                    tar -C /usr/local --strip-components 1 -xzf $i 
+                    npm install -g npm@latest
+                    NODE_VER=$(node -v)
+                    NPM_VER=$(npm -v)
+
+                    echo -ne " Nodejs" "\033[0;32m$NODE_VER\033[0m" "Installed Sucessfully"
+                    echo -ne " Npm" "\033[0;32m$NPM_VER\033[0m" "Installed Sucessfully"
+
+                else
+                    echo "Invalid Extantion"
+
+                fi
+
+            else
+            echo ""
+            echo -ne "\033[0;31mPlease Select Linux-x64 Package URL\033[0m " 
+            echo ""
+            exit;
+
+            fi
 
         else
-            echo "Not Extantion"
-
+            echo ""
+            printf '%s\n' "Invalid URL, Retry Again"
+            echo ""
         fi
-
-    else
-        echo ""
-        echo -ne "\033[0;31mInvalid URL, Retry Again\033[0m"
-        echo ""
-    fi 
 
 fi    
 }    
