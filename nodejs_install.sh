fdf2665b4088259ceb69e7fcf827f987da146a87
Ashwin M.A <ashwin.m@RG-LAP-000240.local>
2020-07-15 20:36:29 +0530

    update

--- nodejs_install.sh
+++ nodejs_install.sh
@@ -8,20 +8,45 @@ if [ `whoami` != root ]; then
         exit
 else    
 
-    read -p "Past URL to Install : " url
+     read -p "Past URL to Install : " url
 
     if  curl --output /dev/null --silent --head --fail "$url"; then
-    
-        i=`echo $url | sed 's|.*/||'`
-        curl -o $i $url
-        tar -C /usr/local --strip-components 1 -xf $i
-        npm install -g npm@latest
+        
+        i=`echo $url | sed 's|.*/||'` 
+        z=`echo $url | sed 's|.*tar.||'`
 
-        NODE_VER=$(node -v)
-        NPM_VER=$(npm -v)
+        if [ "$z" = xz ]
+        then
 
-        echo -ne " Nodejs" "\033[0;32m$NODE_VER\033[0m" "Installed Sucessfully"
-        echo -ne " Npm" "\033[0;32m$NPM_VER\033[0m" "Installed Sucessfully"
+            curl -o $i $url
+            tar -C /usr/local --strip-components 1 -xf $i
+            npm install -g npm@latest
+            rm -rf $i*    
+
+            NODE_VER=$(node -v)
+            NPM_VER=$(npm -v)
+
+            echo -ne " Nodejs" "\033[0;32m$NODE_VER\033[0m" "Installed Sucessfully"
+            echo -ne " Npm" "\033[0;32m$NPM_VER\033[0m" "Installed Sucessfully"
+
+        elif [ "$z" = gz ] 
+        then
+
+            curl -o $i $url
+            tar -C /usr/local --strip-components 1 -xzf $i 
+            npm install -g npm@latest
+            rm -rf $i*
+
+            NODE_VER=$(node -v)
+            NPM_VER=$(npm -v)
+
+            echo -ne " Nodejs" "\033[0;32m$NODE_VER\033[0m" "Installed Sucessfully"
+            echo -ne " Npm" "\033[0;32m$NPM_VER\033[0m" "Installed Sucessfully"
+
+        else
+            echo "Not Extantion"
+
+        fi
 
     else
         echo ""
